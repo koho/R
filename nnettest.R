7bd85cc811a9cd4c52b8276048aabf91c12be50f
koho <ihopeme.hd@gmail.com>
2017-05-03 17:50:16 +0800

    Create nnettest.R

---
+++ nnettest.R
@@ -0,0 +1,54 @@
+library(nnet)
+n = 3
+redwine = read.csv("winequality-red.csv", sep = ';')
+nrowwine = nrow(redwine)
+ncolwine = ncol(redwine)
+redwine$quality = as.factor(redwine$quality)
+if (n == 1)
+  set.seed(123456)
+
+tp_eachcorrects = NULL;
+tp_allcorrects = NULL;
+tp = seq(0.5,0.9,0.1)
+for (tr in tp) {
+eachcorrects = numeric(length(levels(redwine$quality)))
+names(eachcorrects) = levels(redwine$quality)
+eachn = numeric(length(eachcorrects))
+names(eachn) = levels(redwine$quality)
+allcorrects = numeric(n)
+for (i in 1:n) {
+  id = sample(1:nrowwine, nrowwine * tr)
+  winetrain = redwine[id,]
+  winetest = redwine[setdiff(1:nrowwine, id),]
+  ideal = class.ind(winetrain$quality)
+  winenet = nnet(winetrain[, -ncolwine], ideal, size = 30, softmax = TRUE, maxit = 20)
+  winepredict = predict(winenet, winetest[,-ncolwine], 'class')
+  cmat = table(winetest[,ncolwine], winepredict)
+  
+  eachcorrectrate = lapply(rownames(cmat), function(x) {
+    if (x %in% colnames(cmat)) {
+      cmat[x,x] / sum(cmat[x,])
+    } else
+      0
+  })
+  
+  eachcorrects[rownames(cmat)] = eachcorrects[rownames(cmat)] + unlist(eachcorrectrate)
+  eachn[rownames(cmat)] = eachn[rownames(cmat)] + 1
+  
+  allcorrectrate = lapply(rownames(cmat), function(x) {
+    if (x %in% colnames(cmat)) {
+      cmat[x,x]
+    } else
+      0
+  })
+  allcorrects[i] = sum(unlist(allcorrectrate)) / sum(cmat)
+}
+eachcorrects = eachcorrects / eachn
+tp_eachcorrects = rbind(tp_eachcorrects, eachcorrects)
+tp_allcorrects = rbind(tp_allcorrects, allcorrects)
+}
+rownames(tp_eachcorrects) = tp
+rownames(tp_allcorrects) = tp
+par(mfrow = c(1,2))
+plot(tp_allcorrects[,1], type = 'b')
+plot(tp_allcorrects[1,], type = 'b')
